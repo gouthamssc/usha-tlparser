57df6e27875ab63a87ad58142fa12ab4a3e4123c
goutham ssc <gouthamsneham@gmail.com>
2021-05-26 23:40:50 +0530

    tlparser

---
+++ main.py
@@ -0,0 +1,50 @@
+# import regex and datetime
+import re
+from datetime import datetime, timedelta
+
+# Open the log file
+filename="C:\\Users\\0008EI744\\Downloads\\TimeLogCarbon.txt"
+textfile = open(filename, 'r')
+
+firstLine = textfile.readline()
+if re.match(r"Time Log:", firstLine):
+    # Empty list for storing regex output
+    time = []
+    row_matches = []
+    reg = re.compile(
+        "\d{1,2}[:]\d{2}\w{2}\s{0,1}[-]\s{0,1}\d{1,2}[:]\d{2}\w{2}")    # Regex for capturing time with pattern 4:03pm - 6:57pm
+    reg2 = re.compile("\d{1,2}[:]\d{2}\w{2}")                           # Regex for capturing start time and end time with pattern 4:03pm/am
+
+    for line in textfile:
+        if (reg.findall(line)):
+            row_matches = reg.findall(line)
+            for li in row_matches:
+                a = reg2.findall(li)
+                a2 = datetime.strptime(a[0], '%I:%M%p').strftime('%H:%M:%S')
+                a3 = datetime.strptime(a[1], '%I:%M%p').strftime('%H:%M:%S')
+                max2 = datetime.strptime("11:59pm", '%I:%M%p').strftime('%H:%M:%S')
+                if (a2 > a3):  # if hours in start-time >  hours in end-time
+                    delta1 = datetime.strptime(max2, '%H:%M:%S') - datetime.strptime(a2, '%H:%M:%S')
+                    delta = ((delta1 + datetime.strptime(a3, '%H:%M:%S')).strftime('%H:%M:%S'))
+                    delta = datetime.strptime(delta, '%H:%M:%S')
+                    time_change = timedelta(minutes=1)
+                    delta = ((delta + time_change).strftime('%H:%M:%S'))
+                    delta = datetime.strptime(delta, '%H:%M:%S')
+                    exact = delta - datetime(1900, 1, 1)
+                    delta = int(exact.total_seconds())
+                    time.append(delta)
+                    print("Start-time: " + a2, "| End-time: " + a3, "| Time spent: " + str(exact))
+                elif (a3 > a2):  # if hours in start-time < hours in end-time
+                    delta = datetime.strptime(a3, '%H:%M:%S') - datetime.strptime(a2, '%H:%M:%S')
+                    time.append(int(delta.total_seconds()))
+                    print("Start-time: " + a2, "| End-time: " + a3, "| Time spent: " + str(delta))
+                else:  # if hours in start-time ==  hours in end-time
+                    print("Start and End Times are same, so no work happened")
+        else:
+            print("Time not found")
+
+    print("_________________________________________________________________________________________")
+    print("Total Time spent: " + str(timedelta(seconds=sum(time))))
+    textfile.close()
+else:
+    print("File is not as expected")
---
+++ regex.py
@@ -0,0 +1,7 @@
+import datetime
+
+from RegexGenerator import RegexGenerator
+
+myRegexGenerator = RegexGenerator("Time Log:")
+
+print(myRegexGenerator.get_regex())
